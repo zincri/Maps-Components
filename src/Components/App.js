a513dd3e62fa69953a3b4fdefec78a011320e2ae
zincri <zincri_mlz@hotmail.com>
2021-03-01 20:36:14 -0600

    Zincri

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,43 @@ function App() {
   //const [rangos, setRangos] = useState([]);
 
 useEffect(() => {
+  fetch("https://seguimiento.guanajuato.gob.mx/apiinformacionsocial/api/renapo/porcurp/pL@t_1n%7CRun$28/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("RESULT",result);
+          if(result.Mensaje === "OK")
+          {
+            console.log("Curp valida");
+          }
+          else if(result.Mensaje === "La CURP no se encuentra en la base de datos")
+          {
+            console.log("La CURP no se encuentra en la base de datos");
+          }
+          else if(result.Mensaje === "El campo CURP: No cumple con el formato especificado.")
+          {
+            console.log("El campo CURP: No cumple con el formato especificado.");
+          }
+          else{
+            console.log("El servicio no esta disponible.");
+          }
+
+        }
+
+        // Nota: es importante manejar errores aquí y no en 
+        // un bloque catch() para que no interceptemos errores
+        // de errores reales en los componentes.
+        /* (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          }
+          )
+        }*/
+      ).catch((e)=>{
+        console.log("e",e);
+      });
+  
   /* async function getMunicipios() {
     try {
 
